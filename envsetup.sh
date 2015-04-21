02fb36eb22054c140df225546d15242cff59efce
Justin M <zzpianoman@gmail.com>
2015-04-20 22:02:32 -0400

    Revert "Toolchains: Switch to Linaro 4.9 for ROM"
    This reverts commit 605589461ac55816e55798c16bee48a4986a5013.

--- envsetup.sh
+++ envsetup.sh
@@ -157,7 +157,7 @@ function setpaths()
             ;;
         x86_64) toolchaindir=x86/x86_64-linux-android-$targetgccversion/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-linaro-4.9/bin
+        arm) toolchaindir=arm/arm-linux-androideabi-$targetgccversion/bin
             ;;
         arm64) toolchaindir=aarch64/aarch64-linux-android-$targetgccversion/bin;
                toolchaindir2=arm/arm-linux-androideabi-$targetgccversion2/bin
