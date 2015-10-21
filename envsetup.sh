a8ba029ba8dd059b9739a43982436f1b57bbfbe1
Khalid Zubair <kzubair@cyngn.com>
2015-10-21 12:43:14 -0700

    mms: introduce a shortcut to quickly rebuild kernel/boot.img
    Add a new shortcut to short circuit the long build process that walks
    the entire tree collecting all Android.mks. The resulting dependency
    list is sufficient to rebuild the kernel, copy product files and
    repack the system image with updated modules.
    
    This shortcut allows Kernel devs to rebuild just the boot.img and
    kernel modules very quickly (20s vs 3min).
    
    Change-Id: Ie0a69f241ea7b920859ff1e02c3542b79952462c

--- envsetup.sh
+++ envsetup.sh
@@ -10,6 +10,8 @@ Invoke ". build/envsetup.sh" from your shell to add the following functions to y
            To limit the modules being built use the syntax: mmm dir/:target1,target2.
 - mma:     Builds all of the modules in the current directory, and their dependencies.
 - mmma:    Builds all of the modules in the supplied directories, and their dependencies.
+- mms:     Short circuit builder. Quickly re-build the kernel, rootfs, boot and system images
+           without deep dependencies. Requires the full build to have run before.
 - cgrep:   Greps on all local C/C++ files.
 - ggrep:   Greps on all local Gradle files.
 - jgrep:   Greps on all local Java files.
@@ -1717,6 +1719,29 @@ function mka() {
     else
         echo "Couldn't locate the top of the tree.  Try setting TOP."
     fi
+}
+
+function mms() {
+    local T=$(gettop)
+    if [ -z "$T" ]
+    then
+        echo "Couldn't locate the top of the tree.  Try setting TOP."
+        return 1
+    fi
+
+    case `uname -s` in
+        Darwin)
+            local NUM_CPUS=$(sysctl hw.ncpu|cut -d" " -f2)
+            ONE_SHOT_MAKEFILE="__none__" \
+                make -C $T -j $NUM_CPUS "$@"
+            ;;
+        *)
+            local NUM_CPUS=$(cat /proc/cpuinfo | grep "^processor" | wc -l)
+            ONE_SHOT_MAKEFILE="__none__" \
+                mk_timer schedtool -B -n 1 -e ionice -n 1 \
+                make -C $T -j $NUM_CPUS "$@"
+            ;;
+    esac
 }
 
 function reposync() {
